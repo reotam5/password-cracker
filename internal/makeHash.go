d04a7f748413d855220f61ab5926e910cb9e21db
Reo Tamai <41591313+reotam5@users.noreply.github.com>
2025-10-03 22:41:10 -0700

    bug fix

--- internal/makeHash.go
+++ internal/makeHash.go
@@ -6,7 +6,7 @@ import (
 )
 
 func MakeHash(plaintext, algorithm, salt string) (string, error) {
-	hash, err := exec.Command("mkpasswd", "-m", algorithm, "\""+plaintext+"\"", "\""+salt+"\"", "-s").Output()
+	hash, err := exec.Command("mkpasswd", "-m", algorithm, "\""+plaintext+"\"", salt, "-s").Output()
 
 	if err != nil {
 		return "", err

6b8a10fc8a629b7433f1096854a0049cd6e508da
Reo Tamai <41591313+reotam5@users.noreply.github.com>
2025-10-05 20:37:44 -0700

    support for multiple hash algorithms

--- internal/makeHash.go
+++ internal/makeHash.go
@@ -4,6 +4,9 @@ import (
 	"os/exec"
 	"strings"
 
+	"github.com/GehirnInc/crypt"
+	_ "github.com/GehirnInc/crypt/md5_crypt"
+	"github.com/go-crypt/crypt/algorithm/bcrypt"
 	"github.com/openwall/yescrypt-go"
 )
 
@@ -11,11 +14,21 @@ func MakeHash(plaintext string, algorithm string, salt string, parameters string
 	var err error
 	var hash []byte
 
-	// mkpasswd doesn't seem to support yescrypt
-	if algorithm == "yescrypt" {
+	// mkpasswd doesn't seem to support yescrypt, md5, or bcrypt
+	switch algorithm {
+	case "yescrypt":
 		saltBytes := []byte("$y$" + parameters + "$" + salt)
 		hash, err = yescrypt.Hash([]byte(plaintext), saltBytes)
-	} else {
+	case "md5":
+		crypt := crypt.MD5.New()
+		var hashStr string
+		hashStr, err = crypt.Generate([]byte(plaintext), []byte("$1$"+salt))
+		hash = []byte(hashStr)
+	case "bcrypt":
+		hasher, _ := bcrypt.New()
+		digest, _ := hasher.Hash(plaintext)
+		hash = []byte(digest.Encode())
+	default:
 		cmd := exec.Command("mkpasswd", "-m", algorithm, "-s", "-S", salt)
 		cmd.Stdin = strings.NewReader(plaintext)
 		hash, err = cmd.Output()
